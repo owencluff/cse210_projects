f7c61785aa2fe009619d594d6170750c89a0d2ac
owencluff <owencluff@byui.edu>
2024-09-19 08:35:54 -0600

    finished Prep2 project, might be a bit jank

--- csharp-prep/Prep2/Program.cs
+++ csharp-prep/Prep2/Program.cs
@@ -4,6 +4,38 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep2 World!");
+        Console.Write("What is your grade percentage? ");
+        string input = Console.ReadLine();
+        int gradePercentage = int.Parse(input);
+        string letterGrade = string.Empty;
+        if (gradePercentage >= 90) {
+            letterGrade = "A";
+        }
+        else if (gradePercentage >= 80) {
+            letterGrade = "B";
+        }
+        else if (gradePercentage >= 70) {
+            letterGrade = "C";
+        }
+        else if (gradePercentage >= 60) {
+            letterGrade = "D";
+        }
+        else {
+            letterGrade = "F";
+        }
+        int gradePrecision = gradePercentage % 10;
+        if (gradePrecision >= 7 && !(letterGrade == "A" || letterGrade == "F")) {
+            letterGrade += "+";
+        }
+        else if (gradePrecision <= 3 && letterGrade != "F") {
+            letterGrade += "-";
+        }
+        Console.WriteLine($"You have a(n) {letterGrade}.");
+        if (!(letterGrade == "F" || letterGrade == "D")) {
+            Console.WriteLine("You are passing!");
+        }
+        else {
+            Console.WriteLine("You are failing!");
+        }
     }
 }
