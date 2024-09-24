deb914811b5a4a27fa9b48d716817aea2303e1be
owencluff <owencluff@byui.edu>
2024-09-24 08:36:45 -0600

    Started a sandbox madLib project

--- sandbox/Sandbox/Program.cs
+++ sandbox/Sandbox/Program.cs
@@ -4,6 +4,8 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Sandbox World!");
+        Console.WriteLine("What is your favorite color?");
+        string color = Console.ReadLine();
+        Console.WriteLine($"Your favorite color is {color}.");
     }
 }
