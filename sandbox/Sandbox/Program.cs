77afd09952c6a94d98f914b1fe73cb1378806cb6
owencluff <owencluff@byui.edu>
2024-09-24 10:19:52 -0600

    adding the last bits

--- sandbox/Sandbox/Program.cs
+++ sandbox/Sandbox/Program.cs
@@ -4,8 +4,19 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("What is your favorite color?");
-        string color = Console.ReadLine();
-        Console.WriteLine($"Your favorite color is {color}.");
+        Console.WriteLine("Enter the prompted words:");
+        Console.Write("Adjective: ");
+        string adj1 = Console.ReadLine();
+        Console.Write("Verb: ");
+        string verb1 = Console.ReadLine();
+        Console.Write("Exclamation: ");
+        string excl1 = Console.ReadLine();
+        Console.Write("Verb: ");
+        string verb2 = Console.ReadLine();
+        Console.Write("Verb: ");
+        string verb3 = Console.ReadLine();
+        
+
+
     }
 }
