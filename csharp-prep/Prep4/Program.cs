f05c87fc0252d91d01bed98e20a86e3f2471a83e
owencluff <owencluff@byui.edu>
2024-09-24 10:22:50 -0600

    cleaned up prep4 project

--- csharp-prep/Prep4/Program.cs
+++ csharp-prep/Prep4/Program.cs
@@ -4,30 +4,28 @@ class Program
 {
     static void Main(string[] args)
     {
-        List<int> numbers = new List<int>();
+        List<float> numbers = new List<float>();
         Console.WriteLine("Enter a list of numbers, enter 0 when finished.");
-        int number = 1;
-        int total = 0;
+        float number = 1;
+        float total = 0;
         while (number != 0)
         {
             Console.Write("Enter a number: ");
-            number = int.Parse(Console.ReadLine());
+            number = float.Parse(Console.ReadLine());
             if (number != 0)
             {
                 numbers.Add(number);
             }
         }
-        int largest = 0;
-        foreach (int item in numbers){
+        float largest = 0;
+        foreach (float item in numbers){
             total += item;
             if (item > largest){
                 largest = item;
             }
         }
-        float newTotal = total;
         float newCount = numbers.Count;
-        float average = newTotal / newCount;
-        ;
+        float average = total / newCount;
         Console.WriteLine($"{numbers.Count}");
         Console.WriteLine($"The sum is: {total}");
         Console.WriteLine($"The average is: {average}");
