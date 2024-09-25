6b1c3d4d2a05b9e64c30cedc06b63e9c96130b09
owencluff <owencluff@byui.edu>
2024-09-25 11:02:01 -0600

    Finished Prep5 program

--- csharp-prep/Prep5/Program.cs
+++ csharp-prep/Prep5/Program.cs
@@ -1,9 +1,39 @@
 using System;
+using System.Collections.Specialized;
 
 class Program
 {
+    static void DisplayWelcome()
+    {
+        Console.WriteLine("Welcome to the Program!");
+    }
+    static string PromptUserName()
+    {
+        Console.Write("Please enter your name: ");
+        string name = Console.ReadLine();
+        return name;
+    }
+    static int PromptUserNumber()
+    {
+        Console.Write("Please enter your favorite number: ");
+        int num = int.Parse(Console.ReadLine());
+        return num;
+    }
+    static int SquareNumber(int num)
+    {
+        int NewNum = num * num;
+        return NewNum;
+    }
+    static void DisplayResult(int num, string name)
+    {
+        Console.WriteLine($"{name}, the square of your number is {num}");
+    }
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep5 World!");
+        DisplayWelcome();
+        string name = PromptUserName();
+        int num = PromptUserNumber();
+        int NewNum = SquareNumber(num);
+        DisplayResult(NewNum, name);
     }
 }
