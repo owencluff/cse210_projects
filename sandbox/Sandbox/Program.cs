ac7eacb52facb557988bcd20dc5111c4404f4b18
owencluff <owencluff@byui.edu>
2024-09-26 08:12:35 -0600

    a simple list creating program, for testing things

--- sandbox/Sandbox/Program.cs
+++ sandbox/Sandbox/Program.cs
@@ -2,21 +2,30 @@ using System;
 
 class Program
 {
+    static List<string> getWords()
+    {
+        List<string> words = new List<string>();
+        while (true)
+        {
+            Console.Write("Enter a word: ");
+            string word = Console.ReadLine();
+            if (word == string.Empty)
+            {
+                break;
+            }
+            words.Add(word);
+        }
+        return words;
+    }
     static void Main(string[] args)
     {
-        Console.WriteLine("Enter the prompted words:");
-        Console.Write("Adjective: ");
-        string adj1 = Console.ReadLine();
-        Console.Write("Verb: ");
-        string verb1 = Console.ReadLine();
-        Console.Write("Exclamation: ");
-        string excl1 = Console.ReadLine();
-        Console.Write("Verb: ");
-        string verb2 = Console.ReadLine();
-        Console.Write("Verb: ");
-        string verb3 = Console.ReadLine();
-        
-
-
+        Console.WriteLine("Enter a list of words. Enter a blank line when finished.");
+        List<string> words = new List<string>();
+        words = getWords();
+        Console.WriteLine($"There are {words.Count} words in this list.");
+        foreach (string word in words)
+        {
+            Console.WriteLine(word);
+        }
     }
 }
