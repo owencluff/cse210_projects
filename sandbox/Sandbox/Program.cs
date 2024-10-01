f8887a3c5e2eec4009858998ff8ce2ea76bc1baf
owencluff <owencluff@byui.edu>
2024-10-01 08:37:17 -0600

    worked on some in-class projects

--- sandbox/Sandbox/Program.cs
+++ sandbox/Sandbox/Program.cs
@@ -1,35 +1,19 @@
 using System;
+using System.Runtime.InteropServices.Marshalling;
 
 class Program
 {
-    static List<(string, int)> GetWords(string message)
+     static void Main(string[] args)
     {
-        List<(string, int)> groceries = new List<(string, int)>();
-        while (true)
-        {
-            Console.Write(message);
-            string word = Console.ReadLine().ToLower();
-            if (word == "done")
-            {
-                break;
-            }
-            Console.Write("Enter the price: ");
-            int price = int.Parse(Console.ReadLine());
-            groceries.Add((word, price));
-        }
-        return groceries;
-    }
-    static void Main(string[] args)
-    {
-        Console.WriteLine("Enter your grocery list. Enter 'done' when done.");
-        List<(string, int)> groceryList = GetWords("Enter a grocery item: ");
-        Console.WriteLine($"Grocery List ({groceryList.Count()} items)");
-        int totalCost = 0;
-        foreach ((string item, int price) in groceryList)
-        {
-            Console.WriteLine($"* {item}: ${price}");
-            totalCost += price;
-        }
-        Console.WriteLine($"The total price is ${totalCost}");
+       List<string> animals = new List<string>();
+       animals.Add("goat");
+       animals.Add("sheep");
+       animals.Add("cow");
+
+       Console.WriteLine("Animals:");
+       foreach (string animal in animals)
+       {
+        Console.WriteLine($"* {animal}");
+       }
     }
 }
