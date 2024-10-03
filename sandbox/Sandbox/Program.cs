05d2df8275879ff5f004f873d36a2a65efed60d6
owencluff <owencluff@byui.edu>
2024-10-03 08:11:56 -0600

    did some stuff

--- sandbox/Sandbox/Program.cs
+++ sandbox/Sandbox/Program.cs
@@ -5,15 +5,6 @@ class Program
 {
      static void Main(string[] args)
     {
-       List<string> animals = new List<string>();
-       animals.Add("goat");
-       animals.Add("sheep");
-       animals.Add("cow");
-
-       Console.WriteLine("Animals:");
-       foreach (string animal in animals)
-       {
-        Console.WriteLine($"* {animal}");
-       }
+      Console.WriteLine(38 / 10);
     }
 }
