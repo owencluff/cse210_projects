944e958fcbe297b9833ab9653df81eda5742f1e6
owencluff <owencluff@byui.edu>
2024-10-15 08:43:52 -0600

    finished the fraction project

--- prepare/Learning03/Program.cs
+++ prepare/Learning03/Program.cs
@@ -4,6 +4,14 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Learning03 World!");
+        Fraction f1 = new Fraction();
+        Fraction f2 = new Fraction(3);
+        Fraction f3 = new Fraction(2, 5);
+        f1.SetTop(4);
+        f1.SetBottom(67);
+        Console.WriteLine(f1.GetBottom());
+        Console.WriteLine(f1.GetTop());
+        Console.WriteLine(f3.GetDecimalValue());
+        Console.WriteLine(f2.GetFractionString());
     }
 }
