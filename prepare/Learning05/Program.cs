2d75f829c51b6ca4270dccb43a4a496dd64cfbf3
owencluff <owencluff@byui.edu>
2024-11-12 08:12:21 -0700

    finished learning05 project

--- prepare/Learning05/Program.cs
+++ prepare/Learning05/Program.cs
@@ -4,6 +4,16 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Learning05 World!");
+        Square shape1 = new Square(15, "red");
+        Rectangle shape2 = new Rectangle(15, 13, "blue");
+        Circle shape3 = new Circle(15, "green");
+        List<Shape> shapes = [shape1, shape2, shape3];
+
+        foreach (Shape shape in shapes)
+        {
+            Console.WriteLine($"Area: {shape.GetArea()}");
+            Console.WriteLine($"Color: {shape.GetColor()}");
+            Console.WriteLine();
+        }
     }
 }
